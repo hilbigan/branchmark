df4d90855ff661352f5867387890c058937238c3
Aaron Hilbig <darkolan2@gmail.com>
2021-09-21 18:22:05 +0200

    Initial commit

---
+++ branchmark.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+CONFIG_FILE="./branchmark.config"
+LOG_FILE="./branchmark.log"
+set -euo pipefail
+IFS=$'\n\t'
+
+source "$CONFIG_FILE"
+
+echo "Benchmarking ${#branches[@]} branches..."
+
+for name in ${branches[@]}; do
+    if git checkout "$name" >/dev/null 2>&1; then
+        echo "*** Running on branch $name ***"
+        time (run | egrep --line-buffered "${filter}")
+        echo "*** End run on $name ***"
+    else
+        echo "Failed to checkout $name, skipping."; 
+    fi
+done
