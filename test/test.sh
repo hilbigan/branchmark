400e6e88cc29432c569c91435b1e12c1c96a7490
Aaron Hilbig <darkolan2@gmail.com>
2021-09-22 15:34:04 +0200

    Add some comments

--- test/test.sh
+++ test/test.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 set -euo pipefail
+cd "$(dirname "$0")"
+
 TEST_DIR="/tmp/test-repository"
 DATA_DIR="$(cd .. && pwd)"
+
+# set up test directory and copy files
 rm -rf "$TEST_DIR"
 mkdir -p "$TEST_DIR"
+
 cd "$TEST_DIR"
 cp "$DATA_DIR/branchmark.sh" .
 cp "$DATA_DIR/test/branchmark-test.config" ./branchmark.config
+
+# create repo and branches
 git init
 touch some_file other_file
 git add some_file
@@ -14,11 +21,17 @@ git commit -m "Initial commit"
 git checkout -b development
 git add other_file
 git commit -m "Second commit"
+
+# run
 chmod +x branchmark.sh
 ./branchmark.sh
+
+# compare output
 if cmp --silent "$DATA_DIR/test/expected.log" "./testfile.log"; then
     echo "OK"
 else
     echo "FAILED: Program output differs."
 fi
+
+# clean up
 rm -rf "$TEST_DIR"
